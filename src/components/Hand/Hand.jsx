f8466d64ac23a8983a336c22834e92dbd9c35182
briankwok15 <50119982+briankwok15@users.noreply.github.com>
2020-04-13 19:28:10 -0700

    animate

--- src/components/Hand/Hand.jsx
+++ src/components/Hand/Hand.jsx
@@ -4,13 +4,14 @@ import { Score } from '../Score/Score';
 // REFACTOR using SASS?
 // to conditional render black and red cards - hidden and non-hidden cards?
 import './Hand.css';
+import '../../CSS/animate.css';
 
 export const Hand = ({ whoseHand, hand, score, isHidden, totalScore }) => {
   const currentHand = hand.map((card, index) => {
     // css for hidden card
-    let cardClassName = 'card';
+    let cardClassName = 'animated fadeInDown card';
     if (isHidden && index === 1) {
-      cardClassName = 'hidden-card';
+      cardClassName = 'animated fadeInDown hidden-card';
     }
     return (
       <div className={cardClassName} key={index}>
