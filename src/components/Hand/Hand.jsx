9d64729b98d1b4aaf63a7116b27138d20d8a752a
briankwok15 <50119982+briankwok15@users.noreply.github.com>
2020-04-17 16:44:31 -0700

    game container review complete

--- src/components/Hand/Hand.jsx
+++ src/components/Hand/Hand.jsx
@@ -7,7 +7,6 @@ import './Hand.css';
 import '../../CSS/Animate/animate.css';
 
 export const Hand = ({ whoseHand, hand, score, isHidden, totalScore }) => {
-  console.log('hidden', isHidden);
   const currentHand = hand.map((card, index) => {
     // css for hidden card
     let cardClassName = 'animated fadeInDown card';
